1a8579922ef83233c5afbc8de0285601e2aeffb7
Lucas Ribeiro De Oliveira <lucas25011989@gmail.com>
2021-09-09 14:28:26 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+
+const calcular = document.getElementById('calcular');
+
+
+function imc(){
+ const nome = document.getElementById('nome').value;
+ const altura = document.getElementById('altura').value;
+ const peso = document.getElementById('peso').value;
+ const resultado = document.getElementById('resultado');
+
+  if(nome !== '' && altura !== '' && peso !== ''){
+
+     const valorIMC = (peso/(altura*altura)).toFixed(1);
+
+     let classificacao = "";
+
+     if(valorIMC < 18.5){
+         classificacao = 'abaixo do peso'
+     }
+
+     else if (valorIMC < 25){
+         classificacao = 'com peso ideal.Parabens'
+    } 
+
+     else if (valorIMC < 30){
+        classificacao = 'Levemente acima do peso.'
+     }
+
+     else if (valorIMC < 35){
+        classificacao = 'com obesidade grau 1.'
+     }
+
+     else if (valorIMC < 40){
+        classificacao = 'com obesidade grau 2.'
+     }
+    else{
+        classificacao = 'com obesidade grau 3.Cuidado!!!.'
+    }
+
+    
+
+    resultado.textContent =`${nome} seu IMC é ${valorIMC} e voce esta ${classificacao}`
+
+    
+ }
+}
+
+    
+
+
+
+calcular.addEventListener('click', imc)
